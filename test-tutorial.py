294d7b1629782ab2b478ad83cf662a3b3ca92ba1
KimotoYanke <kimotoyanke@gmail.com>
2019-05-13 09:25:36 +0900

    add print

--- test-tutorial.py
+++ test-tutorial.py
@@ -3,7 +3,10 @@ import sys
 try:
     res = subprocess.check_output("python ./hello-world.py")
 except:
+    print("処理に失敗しました")
     sys.exit(1)
 
+print(res)
 if (res != "Hello World!"):
+    print("出力された文字列が一致しません: " + res)
     sys.exit(1)

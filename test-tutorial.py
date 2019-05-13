855f7f6c21fe1b88f9207620030ad564e75f8dc7
KimotoYanke <kimotoyanke@gmail.com>
2019-05-13 09:37:10 +0900

    log

--- test-tutorial.py
+++ test-tutorial.py
@@ -1,12 +1,20 @@
 import subprocess
 import sys
+from logging import basicConfig, getLogger, DEBUG
+basicConfig(level=DEBUG)
+logger = getLogger(__name__)
+logger.debug('hello')
+
 try:
-    res = subprocess.check_output("python ./hello-world.py")
+    res = subprocess.check_output(["python",
+                                   "./hello-world.py"]).decode("utf-8")
 except:
-    print("処理に失敗しました")
+    logger.error("処理に失敗しました")
     sys.exit(1)
 
-print(res)
-if (res != "Hello World!"):
-    print("出力された文字列が一致しません: " + res)
+logger.debug(repr(res))
+
+if (res != "Hello World!\n"):
+
+    logger.error("出力された文字列が一致しません: " + repr(res))
     sys.exit(1)
